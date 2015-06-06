46521eabbc42210c0c9299a19d318ff159c6e7fc
Andrew Kishino <AndrewKishino@gmail.com>
2015-06-06 16:46:34 -0700

    Combined some conditions and commited stringifyJSON after passing tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,30 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+    return obj.toString();
+  }
+  if (typeof(obj) === 'function' || typeof(obj) === 'undefined') {
+    return null;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof(obj) === 'object') {
+    if (Array.isArray(obj)) {
+      return '[' + obj.map(function(o) { return stringifyJSON(o) }).join(",") + ']';
+    } else {
+      var result = [];
+      Object.keys(obj).forEach(function(key) {
+        var val = stringifyJSON(obj[key]);
+        if (val !== null) {
+          result.push('"' + key + '":' + val);
+        }
+      });
+    return "{" + result.join(",") + "}";
+    }
+  }
+}
