fce9cd4875229084405d7cfe89dc60b36602e64f
Andrew Kishino <AndrewKishino@gmail.com>
2015-06-11 10:29:25 -0700

    Commiting getElementsByClassName.js after passing tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  var match = [];
+  var items = document.body;
+
+  var digDeeper = function(element) {
+    if(element.classList){
+      if (element.classList.contains(className)) {
+        match.push(element);
+      }
+    }
+    if (element.hasChildNodes()) {
+      var children = element.childNodes;
+      for (var i = 0; i < children.length; i++) {
+        digDeeper(children[i]);
+      }
+    }
+  };
+  
+  digDeeper(items);
+  return match;
 };
